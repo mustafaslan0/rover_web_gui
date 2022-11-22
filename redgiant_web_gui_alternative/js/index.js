a47cd4f85487f975b398852af6b5fc92cb15ba27
Mustafa Aslan <89737685+mustafaslan0@users.noreply.github.com>
2022-11-22 03:35:31 +0300

    Add files via upload

---
+++ redgiant_web_gui_alternative/js/index.js
@@ -0,0 +1,12 @@
+function myFunction(imgs) {
+    // Get the expanded image
+    var expandImg = document.body;
+    
+    expandImg.style.backgroundImage = "url("+imgs.src+")";
+
+  
+
+  }
+
+  
+
